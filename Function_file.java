0470f9668403cd3a8961cf0e39525fee01b1c717
Oishe-pal <oyshipaul26@gmail.com>
2026-02-13 23:41:19 +0600

    started file functions

---
+++ Function_file.java
@@ -0,0 +1,15 @@
+public class Function_file 
+{
+    GUI gui;
+    public  Function_file(GUI gui)
+    {
+        this.gui=gui;
+    }
+    public void newFile()
+    {
+        gui.textArea.setText("");
+        gui.window.setTitle("New Tab");
+    }
+}
+    
+
