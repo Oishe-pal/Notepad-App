8517d526c60f61d8c38f25589e434a358a2698bf
Oishe-pal <oyshipaul26@gmail.com>
2026-02-14 23:26:12 +0600

    Till Open

--- Function_file.java
+++ Function_file.java
@@ -1,6 +1,11 @@
+import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class Function_file 
 {
     GUI gui;
+    String fileName;
+    String fileAddress;
     public  Function_file(GUI gui)
     {
         this.gui=gui;
@@ -9,6 +14,31 @@ public class Function_file
     {
         gui.textArea.setText("");
         gui.window.setTitle("New Tab");
+    }
+    public void Open()
+    {
+        FileDialog fd=new FileDialog(gui.window,"Open",FileDialog.LOAD );
+        fd.setVisible(true);
+        if(fd.getFile()!=null)
+        {
+            fileName=fd.getFile();
+            fileAddress=fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+        try{
+
+            BufferedReader br=new BufferedReader(new FileReader(fileAddress+fileName));
+            String line=null;
+            while((line=br.readLine())!=null)
+            {
+                gui.textArea.append(line+"\n");
+            }
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("File not found");
+        }
     }
 }
     
