7695b85fa571f60dcaaa011003557b48844d6a52
Oishe-pal <oyshipaul26@gmail.com>
2026-02-17 21:33:58 +0600

    completed file menu

--- Function_file.java
+++ Function_file.java
@@ -1,6 +1,7 @@
 import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 public class Function_file 
 {
     GUI gui;
@@ -14,6 +15,8 @@ public class Function_file
     {
         gui.textArea.setText("");
         gui.window.setTitle("New Tab");
+        fileName=null;
+        fileAddress=null;
     }
     public void Open()
     {
@@ -24,7 +27,9 @@ public class Function_file
             fileName=fd.getFile();
             fileAddress=fd.getDirectory();
             gui.window.setTitle(fileName);
-        }
+
+            
+            gui.textArea.setText("");
         try{
 
             BufferedReader br=new BufferedReader(new FileReader(fileAddress+fileName));
@@ -33,12 +38,60 @@ public class Function_file
             {
                 gui.textArea.append(line+"\n");
             }
+            br.close();
 
         }
         catch(Exception e)
         {
             System.out.println("File not found");
         }
+        }
+    }
+    public void SaveAS()
+    {
+        FileDialog fd=new FileDialog(gui.window,"Save",FileDialog.SAVE);
+        fd.setVisible(true);
+
+        if((fd.getFile())!=null)
+        {
+            fileName=fd.getFile();
+            fileAddress=fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+        try{
+
+            FileWriter fw=new FileWriter(fileAddress+fileName);
+            fw.write(gui.textArea.getText());
+            fw.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("System Error!!!");
+        }
+    }
+    public void Save()
+    {
+        if(fileName==null)
+        {
+            SaveAS();
+        }
+        else
+        {
+            try
+            {
+                  FileWriter fw=new FileWriter(fileAddress+fileName);
+                  fw.write(gui.textArea.getText());
+                  fw.close();
+            }
+            catch(Exception e)
+            {
+                System.out.println("System Error!!!");
+            }
+        }
+    }
+    public void Exit()
+    {
+        System.exit(0);
     }
 }
     
